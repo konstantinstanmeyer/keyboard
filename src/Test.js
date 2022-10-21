7e8b28f080b81a09e6f7f0d31f7e7b04a9fdc387
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-10-20 20:26:39 -0600

    Adding Id'ed words and letters and general game logic

--- src/Test.js
+++ src/Test.js
@@ -3,53 +3,40 @@ import uuid from 'react-uuid';
 import Keyboard from "./Keyboard";
 
 export default function Test(){
-    const [wordsArray, setWordsArray] = useState([])
     const words = ["yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no"]
     const [blur, setBlur] = useState(false)
     const [inputValue, setInputValue] = useState("")
     const [gameState, setGameState] = useState("not started")
-    const [masterText, setMasterText] = useState([])
-    const [totalUserInput, setTotalUserInput] = useState("")
     const inputField = document.querySelector("#typeInput");
-
-    useEffect(() => {
-        setWordsArray(words)
-        createMasterText(words)
-    }, [])
-
-    function createMasterText(words){
-        let lettersArray = []
-        words.forEach((word) => {
-            for (let i = 0; i < word.length; i++) {
-                lettersArray.push(word[i])
-            }
-        })
-        setMasterText(lettersArray)
-    }
+    const [keyDown, setKeyDown] = useState(0)
 
     function iterate(word){
         let letters = []
         for (let i = 0; i < word.length; i++) {
-            letters.push(<letter key={uuid()} className="text-2xl font-mono">{word[i]}</letter>)
+            letters.push(<letter key={uuid()} id={i} className="text-2xl font-mono">{word[i]}</letter>)
         }
         return letters;
     }
 
     function createWords(words, wordIndex){
         return(
-            <div key={wordIndex} className="pl-2 h-8">
+            <div key={wordIndex} id={wordIndex} className="pl-2 h-8">
                 {iterate(words)}
             </div>
         )
     }
 
     function noInput(){
-        let focus = document.querySelector("#typeInput")
-        focus.blur()
         setBlur(true)
     }
 
     let textTimer;
+
+    // useEffect(() => {
+    //     if (gameState == "not started"){
+    //         textTimer = window.setTimeout(noInput, 3000)
+    //     }
+    // }, [keyDown])
 
     if (gameState === "not started"){
         textTimer = window.setTimeout(noInput, 3000)
@@ -100,13 +87,19 @@ export default function Test(){
     //     }
     // }
 
+    function iterateWords(words){
+        let wordsList = []
+        for (let i = 0; i < words.length; i++){
+            wordsList.push(createWords(words[i], i))
+        }
+        return wordsList;
+    }
+
     return (
         <div className="w-1/3 h-32 ml-2 pt-3">
             <div id="test-zone" className={`overflow-hidden h-24 bg-emerald-600 rounded-lg ${blur ? 'blur-sm transition duration-300' : null} relative`} onClick={() => inputField.focus()}>
                 <input id="typeInput" autoFocus className="absolute z-10" onFocus={() => setBlur(false)} onBlur={() => setBlur(true)} value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-                <div id="text-area" className="flex flex-wrap z-0 absolute">{wordsArray.map((word) => {
-                    return createWords(word, wordsArray.indexOf(word))
-                })}
+                <div id="text-area" className="flex flex-wrap z-0 absolute">{iterateWords(words)}
                 </div>
             </div>
             {blur ? <p className="absolute">press any button to refocus</p> : null}
